2e9201422b8dedec5715c3d64863c9f851cea4bb
demianvselko <vselkod@gmail.com>
2025-08-25 22:58:06 -0300

    chore: initialize api-battle project

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,15 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            ["feat", "fix", "chore", "docs", "test"]
+        ],
+        "subject-case": [2, "never", ["start-case", "pascal-case", "upper-case"]],
+        "footer-leading-blank": [1, "always"],
+        "header-max-length": [2, "always", 100],
+        // Validar que incluya el ticket de Jira
+        "references-empty": [2, "never"],
+    }
+};
